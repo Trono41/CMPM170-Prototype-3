5fcd172fcd5c80ff627ab96563f5b32c8185cbf6
Mont464 <amontore@ucsc.edu>
2025-10-29 14:56:01 -0700

    moving side to side

--- Assets/Scripts/TagAssist/PlayerController.cs
+++ Assets/Scripts/TagAssist/PlayerController.cs
@@ -1,31 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Experimental.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
+    public float velocity = 5;
     private Rigidbody rb;
-    [SerializeField] float velocity = 5;
-    [SerializeField] float jumpHeight = 5;
+    public InputAction moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        moveInput.Enable();
         rb = GetComponent<Rigidbody>();
+        rb.maxLinearVelocity = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal") * velocity;
-
-        // Calculate movement direction
-        Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
-
-        // Move the player
-        // Time.deltaTime ensures consistent movement speed across different frame rates
-        rb.linearVelocity = new Vector3(horizontalInput, 0f, 0f);
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, 0);
-        }
+        rb.AddForce(moveInput.ReadValue<float>() * velocity, 0, 0);
     }
+
 }
