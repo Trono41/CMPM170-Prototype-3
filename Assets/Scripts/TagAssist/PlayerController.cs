e955cf52ed86f1569e03a3193ec274f84715a305
Mont464 <amontore@ucsc.edu>
2025-10-30 19:14:05 -0700

    Somewhat working tag mechanic?

--- Assets/Scripts/TagAssist/PlayerController.cs
+++ Assets/Scripts/TagAssist/PlayerController.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour
     public float velocity = 5;
     private Rigidbody rb;
     public InputAction moveInput;
+    public InputAction jumpInput;
+    private bool jumping = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveInput.Enable();
+        jumpInput.Enable();
         rb = GetComponent<Rigidbody>();
         rb.maxLinearVelocity = 5;
     }
@@ -19,6 +22,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         rb.AddForce(moveInput.ReadValue<float>() * velocity, 0, 0);
+        if (jumpInput.triggered && jumping == false)
+        {
+            jumping = true;
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 10f, 0);
+        }
+        if (rb.position.y == 1)
+        {
+            jumping = false;
+        }
     }
 
 }
